e6589fa2be9361118abad74690c6be8dc1f56843
Andreas Klinger <andreas@klinger.io>
2014-12-17 14:00:03 +0100

    Add CSRF protection

--- app/controllers/doorkeeper/application_controller.rb
+++ app/controllers/doorkeeper/application_controller.rb
@@ -1,6 +1,7 @@
 module Doorkeeper
   class ApplicationController < ActionController::Base
     include Helpers::Controller
+    protect_from_forgery with: :exception
 
     helper 'doorkeeper/form_errors'
   end
